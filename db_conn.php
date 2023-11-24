9d75c9d9daddcc42fc4b1cc1db442a00f0caf7ac
medsar21 <mohamed12sar@gmail.com>
2023-11-24 23:14:55 +0100

    hh

---
+++ db_conn.php
@@ -0,0 +1,15 @@
+<?php
+
+$sname= "localhost";
+$unmae= "root";
+$password = "";
+
+$db_name = "brief6";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if (!$conn) {
+	echo "Connection failed!";
+}
+
+?>
